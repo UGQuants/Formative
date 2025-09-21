a47f16fa9e488626dc16ba2034b670f70f809b56
Alasdair B R Stewart <alasdair.b.r.stewart@gmail.com>
2025-09-21 13:26:31 +0100

    Updated list of packages to install

--- R/01_setup.R
+++ R/01_setup.R
@@ -3,7 +3,7 @@
 # Date: 21/09/2025
 # Author: Alasdair B R Stewart
 
-list.of.packages <- c("tidyverse", "broom", "modelsummary", "devtools")
+list.of.packages <- c("tidyverse", "devtools")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 # Wordcount addin if missing
